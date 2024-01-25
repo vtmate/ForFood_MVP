ba11a19240436e3daa1656a7db451f193d300465
Varga-Toth Mate <vargatothmate@gmail.com>
2024-01-25 01:37:35 +0100

    basic cart function works

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
+function resetPage(){
+    localStorage.setItem('points', 20);
+    progress.style.width = 20 + '%';
+    localStorage.setItem('cart', []);
+}
+
+function addToCart(){
+
+    var cartstring = localStorage.getItem('cart') || JSON.stringify([]);
+    var cart = JSON.parse(cartstring);
+    cart.push(g);
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    var points = localStorage.getItem('points') || 20;
+    points++;
+    localStorage.setItem('points', points);
+    progress.style.width = points + '%';
+    
+}
+
+
 const progress = document.querySelector('.progress');
+var points = localStorage.getItem('points') || 20;
+progress.style.width = points + '%';
 
 
-
-var width = 30;
-progress.style.width = width + '%';
+const content = document.querySelector('.content');
+const reset = document.createElement('button');
+reset.classList.add('reset');
+reset.addEventListener("click", resetPage);
+reset.innerText = "Reset";
+content.append(reset);
