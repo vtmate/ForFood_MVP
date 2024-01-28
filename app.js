5360003c5fbac5bdb7cb4391e6e66c6fe1c03abb
Varga-Toth Mate <vargatothmate@gmail.com>
2024-01-28 20:56:04 +0100

    basic star system works

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 function resetPage(){
     localStorage.setItem('points', 20);
+    localStorage.setItem('allpoints', 20)
     progress.style.width = 20 + '%';
     localStorage.setItem('cart', []);
+    location.reload();
 }
 
 function addToCart(){
@@ -10,18 +12,22 @@ function addToCart(){
     var cart = JSON.parse(cartstring);
     cart.push(g);
     localStorage.setItem('cart', JSON.stringify(cart));
-
-    var points = localStorage.getItem('points') || 20;
-    points++;
-    localStorage.setItem('points', points);
-    progress.style.width = points + '%';
-    
 }
 
+function buyItems(priceSum){
+    var allpoints = localStorage.getItem('points') || 20;
+    allpoints = parseInt(allpoints) + parseInt(priceSum);
+    localStorage.setItem('allpoints', allpoints);
+
+    var points = localStorage.getItem('points') || 20;
+    points = parseInt(points) + parseInt(priceSum);
+    localStorage.setItem('points', points);
+    progress.style.width = points + '%';
+}
 
 const progress = document.querySelector('.progress');
-var points = localStorage.getItem('points') || 20;
-progress.style.width = points + '%';
+var allpoints = localStorage.getItem('allpoints') || 20;
+progress.style.width = allpoints + '%';
 
 
 const content = document.querySelector('.content');
