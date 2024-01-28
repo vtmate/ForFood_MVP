53d01758d348c8f7ddd0f5fe54dc5b023e51faf6
Varga-Toth Mate <vargatothmate@gmail.com>
2024-01-29 00:01:52 +0100

    updated frontend

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function resetPage(){
-    localStorage.setItem('points', 20);
-    localStorage.setItem('allpoints', 20)
+    localStorage.setItem('points', 40);
+    localStorage.setItem('allpoints', 40)
     progress.style.width = 20 + '%';
     localStorage.setItem('cart', []);
     location.reload();
@@ -15,19 +15,27 @@ function addToCart(){
 }
 
 function buyItems(priceSum){
-    var allpoints = localStorage.getItem('points') || 20;
+    var allpoints = localStorage.getItem('points') || 40;
     allpoints = parseInt(allpoints) + parseInt(priceSum);
     localStorage.setItem('allpoints', allpoints);
+    if(allpoints/2 > 100){
+        progress.style.width = '100%';
+    } else {
+        progress.style.width = allpoints/2 + '%';
+    }
 
-    var points = localStorage.getItem('points') || 20;
+    var points = localStorage.getItem('points') || 40;
     points = parseInt(points) + parseInt(priceSum);
     localStorage.setItem('points', points);
-    progress.style.width = points + '%';
 }
 
 const progress = document.querySelector('.progress');
-var allpoints = localStorage.getItem('allpoints') || 20;
-progress.style.width = allpoints + '%';
+var allpoints = localStorage.getItem('allpoints') || 40;
+if(allpoints/2 > 100){
+    progress.style.width = '100%';
+} else {
+    progress.style.width = allpoints/2 + '%';
+}
 
 
 const content = document.querySelector('.content');
